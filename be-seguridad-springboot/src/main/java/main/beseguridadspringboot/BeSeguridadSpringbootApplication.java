ed99e0e5ae5982a6429c96938be705b2c400511e
Carlos-Lopez <carlosedm97@gmail.com>
2022-11-16 18:25:42 -0500

    Inicio del componente de seguridad

---
+++ be-seguridad-springboot/src/main/java/main/beseguridadspringboot/BeSeguridadSpringbootApplication.java
@@ -0,0 +1,15 @@
+package main.beseguridadspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSeguridadSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSeguridadSpringbootApplication.class, args);
+	}
+
+}
+
+// Quedé en la hora 2:30:00
---
+++ be-seguridad-springboot/src/test/java/main/beseguridadspringboot/BeSeguridadSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package main.beseguridadspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSeguridadSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
